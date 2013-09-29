188238189d54cf8cceeb9fb5a420be31b287935b
zozooo <zozooo@gmail.com>
2013-09-29 02:25:46 -0500

    added framework

---
+++ FrameWork/Database/Connection/ConnectionType.cs
@@ -0,0 +1,21 @@
+﻿
+namespace FrameWork
+{
+    // Liste des connexions supportées
+    public enum ConnectionType
+    {
+        DATABASE_XML,
+
+        // Mysql Serveur
+        DATABASE_MYSQL,
+
+        // MicroSoft SQL Serveur
+        DATABASE_MSSQL,
+
+       // ODBC database serveur
+        DATABASE_ODBC,
+
+        // OLE database serveur
+        DATABASE_OLEDB
+    }
+}
